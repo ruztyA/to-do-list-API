e2e98f6cbc1ec8a68a69021a28609a5a687df381
Anggita Ruzty <83459271+ruztyA@users.noreply.github.com>
2021-06-02 16:01:36 +0700

    update category services

--- index.js
+++ index.js
@@ -1,36 +1,72 @@
-const Sequelize = require('sequelize');
-const { isEmpty } = require('lodash');
-const connection = require('../../postgresqlconnection');
-
-const { Model, Op } = Sequelize;
+const Category = require('../../../models/Category/index');
+const { Op } = require('sequelize');
 
 /**
-|-------------------------------------------------------------
-| Category  Model.
-|-------------------------------------------------------------
-*/
+ * Add New Category
+ * @param {String} value
+ * @returns
+ */
+const addNewCategory = async (value) => {
+  const result = await Category.Create({ name: value });
+  return result;
+};
 
-const Category = Model;
+/**
+ * Get all category
+ * @returns {Object[]}
+ */
+const getCategories = async () => {
+  const categories = await Category.findAll();
+  return categories;
+};
 
-Category.init({
-  id: {
-    type: Sequelize.UUID,
-    defaultValue: Sequelize.UUIDV4,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  deletedAt: {
-    type: Sequelize.DATE,
-    defaultValue: null,
-    allowNull: true
-  }
-}, {
-  sequelize: connection,
-  modelName: 'Category',
-  timestamps: true,
-  underscored: true,
-  freezeTableName: true,
-  tableName: 'category'
-})
+/**
+ * Get specific category based on Id
+ * @param {String} categoryId
+ * @returns {Object}
+ */
+const getCategory = async (categoryId) => {
+  const category = await Category.findOne({
+    where: { id: { [Op.eq]: categoryId } }
+  });
 
-export default Category;
+  return category;
+}
+
+/**
+ * Add update category
+ * @param {String} id
+ * @param {String} categoryName
+ * @returns {Object}
+ */
+const updateCategory = async (id, categoryName) => {
+  const category = await Category.update(
+    { name: categoryName },
+    { where: { id: { [Op.eq]: id } } }
+  );
+  return category;
+};
+
+/**
+ * Delete specific category by Id
+ * @param {String} id
+ * @returns {Object}
+ */
+const deleteCategory = async (id) => {
+  const deleteDateTime = new Date();
+
+  const category = await Category.findOne({
+    where: { id: { [Op.eq]: id } }
+  });
+
+  category.deletedAt = deleteDateTime;
+  category.save();
+
+  // const category = await Category.update(
+  //     { deteledAt: deleteDateTime },
+  //     { where: { id: { [Op.eq]: id }}}
+  //     )
+  return category;
+}
+
+module.exports = { addNewCategory, getCategories, getCategory, updateCategory, deleteCategory };
